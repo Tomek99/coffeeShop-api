dfd9213067f31c6aa56cceb43dd4b434c1d6fcd6
tomek <tomaszskupien06@gmail.com>
2023-06-29 19:02:37 +0200

    update

--- server.js
+++ server.js
@@ -25,16 +25,13 @@ app.use(
   })
 );
 
-app.use("/.netlify/functions/api/users", require("./routes/usersRoutes"));
-app.use("/.netlify/functions/api/products", require("./routes/productsRoutes"));
-app.use(
-  "/.netlify/functions/api/addresses",
-  require("./routes/addressesRoutes")
-);
-app.use("/.netlify/functions/api/invoices", require("./routes/invoicesRoutes"));
-app.use("/.netlify/functions/api/stripe", require("./routes/stripeRoutes"));
-app.use("/.netlify/functions/api/orders", require("./routes/ordersRoutes"));
-app.use("/.netlify/functions/api/reviews", require("./routes/reviewsRoutes"));
+app.use("/api/users", require("./routes/usersRoutes"));
+app.use("/api/products", require("./routes/productsRoutes"));
+app.use("/api/addresses", require("./routes/addressesRoutes"));
+app.use("/api/invoices", require("./routes/invoicesRoutes"));
+app.use("/api/stripe", require("./routes/stripeRoutes"));
+app.use("/api/orders", require("./routes/ordersRoutes"));
+app.use("/api/reviews", require("./routes/reviewsRoutes"));
 
 app.use(errorHandler);
 
